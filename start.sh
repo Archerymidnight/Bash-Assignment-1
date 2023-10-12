c1b143e179e723e364be96edde35679b9fb36a3a
Hannah Kiernan <2542983@dundee.ac.uk>
2023-10-12 02:23:25 +0100

    Removed newlines

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-echo "Welcome to the Codebase Management System\n
-\n
-Which option would you like to use today?\n
-1: Make a new repository\n
-2: Check files into existing repository\n
-3: Check files out of existing repository\n"
+echo "Welcome to the Codebase Management System
+
+Which option would you like to use today?
+1: Make a new repository
+2: Check files into existing repository
+3: Check files out of existing repository"
