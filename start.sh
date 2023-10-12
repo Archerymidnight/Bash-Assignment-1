e4d49dae02112b98c00b41bd395a8745c7baab4f
Hannah Kiernan <2542983@dundee.ac.uk>
2023-10-12 02:21:18 +0100

    Initial commit, menu

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Welcome to the Codebase Management System\n
+\n
+Which option would you like to use today?\n
+1: Make a new repository\n
+2: Check files into existing repository\n
+3: Check files out of existing repository\n"
