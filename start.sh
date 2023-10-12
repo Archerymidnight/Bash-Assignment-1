039f8bd9475d518d05d2b42054c5b74f8de74910
Hannah Kiernan <2542983@dundee.ac.uk>
2023-10-12 04:36:19 +0100

    Add files to repository

--- start.sh
+++ start.sh
@@ -1,30 +1,56 @@
-echo "Welcome to the Codebase Management System
-
-Which option would you like to use today?
-1: Make a new repository
-2: Check files into existing repository
-3: Check files out of existing repository
-0: Exit"
-
-read menuChoice
-
-if [ $menuChoice = 1 ]
-then 
-
-echo "Enter the name of the new repository you wish to create
-"
-
-read repName
-
-mkdir $repName || echo "A repository with this name already exists"
+while true; do 
 
 
+    echo "Welcome to the Codebase Management System
 
+    Which option would you like to use today?
+    1: Make a new repository
+    2: Check files into existing repository
+    3: Check files out of existing repository
+    0: Exit"
 
-elif [ $menuChoice = 0 ]
+    read menuChoice
 
-then
+    if [ $menuChoice = 1 ]
+    then 
 
-exit
+        echo "Enter the name of the new repository you wish to create
+        "
 
-fi
+        read repName
+
+        mkdir $repName 2>/dev/null || echo "A repository with this name already exists"
+
+        echo "Enter the name of the initial file for the repository"
+
+        read fileName
+
+        add_file "$fileName"
+
+        while true; do
+
+            echo "Do you wish to add another file? Y/N"
+
+            read newFileChoice
+
+            if [${newFileChoice^^} = 'Y']
+            then
+                echo "Enter the name of the new file"
+
+                read fileName
+
+                add_file "$fileName"
+
+        done
+
+    elif [ $menuChoice = 0 ]
+    then
+
+        exit
+
+    fi
+done
+add_file () {
+    touch $1
+    cd ..
+}
