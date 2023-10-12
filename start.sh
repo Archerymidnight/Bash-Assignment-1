17353ce51af48cb8f485f1fa1bca483554c5c237
Hannah Kiernan <2542983@dundee.ac.uk>
2023-10-12 02:32:15 +0100

    Function (make directory)

--- start.sh
+++ start.sh
@@ -3,4 +3,16 @@ echo "Welcome to the Codebase Management System
 Which option would you like to use today?
 1: Make a new repository
 2: Check files into existing repository
-3: Check files out of existing repository"
+3: Check files out of existing repository
+0: Exit"
+
+read menuChoice
+
+if [$menuChoice = 1]; then 
+
+echo "Enter the name of the new repository you wish to create
+"
+
+read repName
+
+mkdir $repName || echo "A repository with this name already exists"
