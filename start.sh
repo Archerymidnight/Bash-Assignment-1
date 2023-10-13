79a5bfc327af5e88d0b20b4d5acbcc5782329951
Hannah Kiernan <2542983@dundee.ac.uk>
2023-10-13 06:08:44 +0100

    Function to check if credentials are valid

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ log_entry () {
     fi
 
 
-    echo $(date) >> "log.txt"
+    date >> "log.txt"
     echo "Please enter the details of your edit"
 
     read logText
@@ -24,69 +24,111 @@ log_entry () {
     
 }
 
-while true; do 
+valid_login () {
+    local input="testLogins.txt"
+    while read -r line
+    do
+        IFS=' ' read -ra loginArray <<< "$line" 
+
+        if [ "${loginArray[0]}" = "$1" ]
+        then
+            if [ "${loginArray[1]}" = "$2" ]
+            then
+                return 1
+            fi
+        else
+            return 0
+        fi
+    done < "$input"
+}
+
+echo "Please enter your username"
+read username
+echo "Please enter your password"
+read password
+
+validLogin=valid_login "$username" "$password"
+
+if [ "$validLogin" ]
+then
+        
+
+    while true; do 
 
 
-    echo "Welcome to the Codebase Management System
+        echo "Welcome to the Codebase Management System
 
-    Which option would you like to use today?
-    1: Make a new repository
-    2: Check files into existing repository
-    3: Check files out of existing repository
-    0: Exit"
+        Which option would you like to use today?
+        1: Make a new repository
+        2: Check files into existing repository
+        3: Check files out of existing repository
+        0: Exit"
 
-    read menuChoice
+        read menuChoice
 
-    if [ "$menuChoice" = 1 ]
-    then 
+        if [ "$menuChoice" = 1 ]
+        then 
 
-        echo "Enter the name of the new repository you wish to create
-        "
+            echo "Enter the name of the new repository you wish to create
+            "
 
-        read repName
+            read repName
 
-        mkdir "$repName" 2>/dev/null || echo "A repository with this name already exists"
+            mkdir "$repName" 2>/dev/null || echo "A repository with this name already exists"
 
-        if [ -d "$repName" ]
+            if [ -d "$repName" ]
+            then
+
+                echo "Enter the name of the initial file for the repository"
+
+                read fileName
+
+                add_file "$fileName"
+
+                while true; do
+
+                    echo "Do you wish to add another file? Y/N"
+
+                    read newFileChoice
+
+                    if [ "${newFileChoice^^}" = 'Y' ]
+                    then
+                        echo "Enter the name of the new file"
+
+                        read fileName
+
+                        add_file "$fileName"
+                    elif [ "${newFileChoice^^}" == "N" ]
+                    then
+                        chgrp -R checkout "$repName"
+                        chmod -R 770 "$repName"
+                        break
+                    
+                    else
+                    echo "Invalid input, please try again
+
+                    "
+
+                    fi
+                done
+            fi
+
+        elif [ "$menuChoice" = 2 ]
         then
 
-            echo "Enter the name of the initial file for the repository"
 
-            read fileName
+        elif [ "$menuChoice" = 3 ]
+        then
 
-            add_file "$fileName"
+        
 
-            while true; do
+        elif [ "$menuChoice" = 0 ]
+        then
+            valid_login
+            exit
 
-                echo "Do you wish to add another file? Y/N"
+        fi
 
-                read newFileChoice
-
-                if [ "${newFileChoice^^}" = 'Y' ]
-                then
-                    echo "Enter the name of the new file"
-
-                    read fileName
-
-                    add_file "$fileName"
-                elif [ "${newFileChoice^^}" == "N" ]
-                then
-                    chgrp -R checkout "$repName"
-                    chmod -R 770 "$repName"
-                    break
-                
-                else
-                echo "Invalid input, please try again
-
-                "
-
-                fi
-            done
-    fi
-    elif [ "$menuChoice" = 0 ]
-    then
-
-        exit
-
-    fi
-done
+    done
+else
+    echo "Invalid credentials. Try again"
